1c713d3f40a885c0cac7ee65a7396227eb25bcb0
Emil Hägglund <haegglund.emil@gmail.com>
2016-11-23 15:54:05 +0100

    Moved x2fa.py to CanSNPer

--- setup.py
+++ setup.py
@@ -6,22 +6,18 @@ if sys.version_info[:2] != (2, 7):
     sys.exit('Sorry, only Python 2.7 is supported')
 
 setup(
-    name = "CanSNPer",
-    version = "1.0.6",
-    ulr = "https://github.com/adrlar/CanSNPer",
-    description = "CanSNPer: A toolkit for SNP-typing using NGS data.",
-    license = "GPL'",
-    keywords = "Bioinformatics SNP-typing sequence-data",
-    classifiers = [
+    name="CanSNPer",
+    version="1.0.6",
+    url="https://github.com/adrlar/CanSNPer",
+    description="CanSNPer: A toolkit for SNP-typing using NGS data.",
+    license="GPL'",
+    keywords="Bioinformatics SNP-typing sequence-data",
+    classifiers=[
         'Development Status :: 5 - Alpha',
         'License :: OSI Approved :: GPL',
         'Programming Language :: Python :: 2.7'
         ],
-    install_requires = ['numpy', 'ete2'],
-    packages = ['CanSNPer'],
-    entry_points = {
-        'console_scripts':[
-            'CanSNPer=CanSNPer.__main__:main'
-        ]
-    }
-)
+    install_requires=['numpy', 'ete2'],
+    packages=['CanSNPer'],
+    scripts=['CanSNPer/x2fa.py'],
+    entry_points={'console_scripts': ['CanSNPer=CanSNPer.__main__:main']})
